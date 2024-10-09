93bf4213dbf5d398c92f3e8a6df0e2c04a7a2cb8
fatiihvarol <fatihvrl38@gmail.com>
2024-10-09 12:57:37 +0300

    to-do item cqrs commands and queries added

--- to-do-application-web-api/Business/Cqrs/TodoItemCqrs.cs
+++ to-do-application-web-api/Business/Cqrs/TodoItemCqrs.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.OpenApi.Any;
 using to_do_application_web_api.Base.Response;
 using to_do_application_web_api.Data.Schema;
 using to_do_application_web_api.Data.Schema.Auth;
@@ -6,6 +7,14 @@ using to_do_application_web_api.Data.Schema.Auth;
 namespace to_do_application_web_api.Business.Cqrs
 {
     public record CreateTodoItemCommand(TodoItemRequest Model) : IRequest<ApiResponse<TodoItemResponse>>;
+    public record UpdateTodoItemCommand(int Id ,TodoItemRequest Model) : IRequest<ApiResponse<TodoItemResponse>>;
+    public record UpdateStatusCommand(int Id ) : IRequest<ApiResponse<string>>;
+    public record DeletetodoItemCommand(int Id ) : IRequest<ApiResponse<string>>;
+
+
+
+    public record GetMyTodoItems() : IRequest<ApiResponse<List<TodoItemResponse>>>;
+    public record GetTodoItemById(int Id) : IRequest<ApiResponse<TodoItemResponse>>;
 
 
 
