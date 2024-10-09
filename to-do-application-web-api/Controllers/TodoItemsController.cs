1e80675aed72b1e36e017fdb7918a3fd2d7d5fc7
fatiihvarol <fatihvrl38@gmail.com>
2024-10-09 12:58:25 +0300

    to-do item controller updated

--- to-do-application-web-api/Controllers/TodoItemsController.cs
+++ to-do-application-web-api/Controllers/TodoItemsController.cs
@@ -14,13 +14,45 @@ namespace to_do_application_web_api.Controllers
 
         public TodoItemsController(IMediator mediator)
         {
-            
+
             _mediator = mediator;
         }
-        [HttpPost]
+        [HttpPost("Create")]
         public async Task<IActionResult> CreateTodoItem([FromBody] TodoItemRequest request)
         {
             var response = await _mediator.Send(new CreateTodoItemCommand(request));
+            return Ok(response);
+        }
+        [HttpPut("Update/{id}")]
+        public async Task<IActionResult> UpdateTodoItem(int id, [FromBody] TodoItemRequest request)
+        {
+            var response = await _mediator.Send(new UpdateTodoItemCommand(id, request));
+            return Ok(response);
+        }
+        [HttpPut("UpdateStatus/{id}")]
+        public async Task<IActionResult> UpdateStatus(int id)
+        {
+            var response = await _mediator.Send(new UpdateStatusCommand(id));
+            return Ok(response);
+        }
+        [HttpGet("GetMyTodoItems")]
+        public async Task<IActionResult> GetMyTodoItems()
+        {
+            var response = await _mediator.Send(new GetMyTodoItems());
+            return Ok(response);
+        }
+
+        [HttpGet("GetTodoItemById/{id}")]
+        public async Task<IActionResult> GetTodoItemById(int id)
+        {
+            var response = await _mediator.Send(new GetTodoItemById(id));
+            return Ok(response);
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> DeleteTodoItem(int id)
+        {
+            var response = await _mediator.Send(new DeletetodoItemCommand(id));
             return Ok(response);
         }
     }
